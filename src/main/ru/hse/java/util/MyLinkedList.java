56b67a67ca5bab1c17c9b7711fd5d71f9b674ffe
annapotr <potryasaeva.anna@mail.ru>
2023-02-01 23:03:39 +0100

    Remove error

--- src/main/ru/hse/java/util/MyLinkedList.java
+++ src/main/ru/hse/java/util/MyLinkedList.java
@@ -37,7 +37,7 @@ public class MyLinkedList<E> extends AbstractCollection<E> {
             }
 
             @Override
-            public E next() throws IllegalStateException {
+            public E next() {
                 E value = cur.next.value;
                 if (value == null) throw new NullPointerException();
                 cur = cur.next;
