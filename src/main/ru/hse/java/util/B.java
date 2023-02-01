e4329f5544b8b06a9d8120ef618f38c64804b42d
annapotr <potryasaeva.anna@mail.ru>
2023-02-02 00:40:02 +0100

    merge classes???

--- src/main/ru/hse/java/util/B.java
+++
@@ -1,8 +0,0 @@
-package ru.hse.java.util;
-
-public class B {
-    public int b(int x, int y) {
-        System.out.println(x);
-        return x * y;
-    }
-}
--- src/main/ru/hse/java/util/C.java
+++ src/main/ru/hse/java/util/C.java
@@ -1,8 +1,12 @@
 package ru.hse.java.util;
 
-public class A {
+public class C {
     public int a(int x, int y) {
         System.out.println(y);
         return x + y;
+    }
+    public int b(int x, int y) {
+        System.out.println(x);
+        return x * y;
     }
 }
