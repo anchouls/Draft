feb1a3f59f2c0545b0822eb9ad1e057b1543bdec
annapotr <potryasaeva.anna@mail.ru>
2023-02-02 19:39:07 +0100

    Added A class

--- src/main/ru/hse/java/util/C.java
+++ src/main/ru/hse/java/util/C.java
@@ -1,18 +1,9 @@
 package ru.hse.java.util;
 
-import java.util.HashMap;
 
 public class C {
-    protected final static HashMap<Integer, String> cmd2String = new HashMap();
-    static {
-        cmd2String.put(1, "conf");
-        cmd2String.put(2, "cons");
-        cmd2String.put(3, "crst");
-        cmd2String.put(4, "dump");
-        cmd2String.put(5, "envi");
-    }
     public String b(int x, int y) {
-        String cmd = cmd2String.get(3);
+        String cmd = A.cmd2String.get(3);
         return cmd;
     }
 }
