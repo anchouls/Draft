5f95ee19c60283f654986701ecd561be324b34fe
annapotr <potryasaeva.anna@mail.ru>
2023-02-02 19:37:54 +0100

    Update

--- src/main/ru/hse/java/util/C.java
+++ src/main/ru/hse/java/util/C.java
@@ -1,12 +1,18 @@
 package ru.hse.java.util;
 
+import java.util.HashMap;
+
 public class C {
-    public int a(int x, int y) {
-        System.out.println(y);
-        return x + y;
+    protected final static HashMap<Integer, String> cmd2String = new HashMap();
+    static {
+        cmd2String.put(1, "conf");
+        cmd2String.put(2, "cons");
+        cmd2String.put(3, "crst");
+        cmd2String.put(4, "dump");
+        cmd2String.put(5, "envi");
     }
-    public int b(int x, int y) {
-        System.out.println(x);
-        return x * y;
+    public String b(int x, int y) {
+        String cmd = cmd2String.get(3);
+        return cmd;
     }
 }
