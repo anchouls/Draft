03a0a7d92a233643835331cb535626e3204752da
annapotr <potryasaeva.anna@mail.ru>
2023-02-02 21:40:09 +0100

    Inline attribute???

--- src/main/ru/hse/java/util/C.java
+++ src/main/ru/hse/java/util/C.java
@@ -5,10 +5,9 @@ public class C {
     private int a = 5;
     private int b = 66;
     private int c = 7;
-    private int d = (c == 7) ? b : c;
     private int e = 4;
     private int k = 22;
-    private int x = (e == 4) ? k : d;
+    private int x = (e == 4) ? k : ((c == 7) ? b : c);
     public int b(int x, int y) {
         String cmd = A.cmd2String.get(3);
         int z = x + 5;
