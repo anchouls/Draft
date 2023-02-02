e07fb0d80fe68ae9bb9e8953f522228df855c2b1
annapotr <potryasaeva.anna@mail.ru>
2023-02-02 21:39:04 +0100

    Update

--- src/main/ru/hse/java/util/C.java
+++ src/main/ru/hse/java/util/C.java
@@ -2,8 +2,16 @@ package ru.hse.java.util;
 
 
 public class C {
-    public String b(int x, int y) {
+    private int a = 5;
+    private int b = 66;
+    private int c = 7;
+    private int d = (c == 7) ? b : c;
+    private int e = 4;
+    private int k = 22;
+    private int x = (e == 4) ? k : d;
+    public int b(int x, int y) {
         String cmd = A.cmd2String.get(3);
-        return cmd;
+        int z = x + 5;
+        return z;
     }
 }
