acad58d88dfc5c487408955dc2dfa1b6bb88b4a7
Anna Potriasaeva <anchouls@Annas-MacBook-Pro.local>
2023-04-21 21:30:03 +0200

    Added class A

---
+++ src/main/ru/hse/java/util/A.java
@@ -0,0 +1,18 @@
+package ru.hse.java.util;
+
+public class A {
+
+    int first = 1;
+    int second = 2;
+    int third = 3;
+
+    public int methodA1() {
+        int a = first + second;
+        return a;
+    }
+
+    public int methodA2() {
+        int b = first + third;
+        return b;
+    }
+}
